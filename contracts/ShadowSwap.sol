fb51886857a522f8afd223dd7913882c6b3b54ac
last mohican <lastmohicans@protonmail.com>
2020-10-06 18:02:30 +0300

    new req

--- contracts/ShadowSwap.sol
+++ contracts/ShadowSwap.sol
@@ -372,94 +372,114 @@ contract SolRsaVerify {
 
 }
 
-
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address payable) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
+interface IERC20 {
     /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
+     * @dev Returns the amount of tokens in existence.
      */
-    constructor () internal {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
+    function totalSupply() external view returns (uint256);
 
     /**
-     * @dev Returns the address of the current owner.
+     * @dev Returns the amount of tokens owned by `account`.
      */
-    function owner() public view returns (address) {
-        return _owner;
-    }
+    function balanceOf(address account) external view returns (uint256);
 
     /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(_owner == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
      *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
      */
-    function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
+    function transfer(address recipient, uint256 amount) external returns (bool);
 
     /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
 
 contract ShadowHarvester is Ownable, SolRsaVerify {
     //using SolRsaVerify for bytes32;
-
+    using SafeMath for uint;
 
     bool[] private enableKeys;
 
     bytes[] private availableKeys;
 
+    mapping(address => uint256) lastHarvestSaveBlock;
 
-    constructor() public {
+    mapping(address => uint256) harvestSum;
 
+    address public token;
+
+    constructor(address _token) public {
+        token = _token;
     }
 
     function harvest(uint256 i,
         uint256 amount,
         uint256 lastBlockNumber,
         uint256 currentBlockNumber,
-        bytes memory signi) public  {
+        bytes memory signi) public {
         bytes32 _data = keccak256(abi.encode(amount, lastBlockNumber, currentBlockNumber));
         bytes memory _e;
-        require(pkcs1Sha256Verify(_data, signi, _e, availableKeys[i]) == 0, "Incorrect data");
-        // 
-        address _sender; // = 
+        address userAddress;
+        uint256 savedBlockInData;
 
+        require(pkcs1Sha256Verify(_data, signi, _e, availableKeys[i]) == 0, "Incorrect data");
+        //require(userAddress == msg.sender, "Message sender isn't signer");
+        //require(savedBlockInData == lastBlockNumber, "")
+        require(currentBlockNumber < block.number, " ");
+
+        //IERC20(token).transferFrom(this, _sender, amount);
+        lastHarvestSaveBlock[userAddress] = currentBlockNumber;
+        harvestSum[userAddress] = harvestSum[userAddress].add(amount);
     }
 
 
